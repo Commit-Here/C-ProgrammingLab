6372688923db2f5e33aa077a93a9a1d437345e7a
Commit-Here <burhanqibal86@gmail.com>
2021-11-19 11:48:59 +0530

    Added Comments

--- Name.c
+++ Name.c
@@ -1,6 +1,7 @@
 // Program to display Name on the screen
 #include<stdio.h>
 
+// main funciton is always executed first.
 void main()
 {
     // printf is a function predefined in c library(standard input and output).
