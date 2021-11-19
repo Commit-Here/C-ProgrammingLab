9ff9073fd3201cec623c1b4e16ead8cad35b3c66
Commit-Here <burhanqibal86@gmail.com>
2021-11-19 11:46:42 +0530

    First Program

---
+++ Name.c
@@ -0,0 +1,8 @@
+// Program to display Name on the screen
+#include<stdio.h>
+
+void main()
+{
+    // printf is a function predefined in c library(standard input and output).
+    printf("My name is Burhan/nI am from Department Of CSE");
+} // main
