8c371470f751da5be1ccfe582643a46d51e0a9f2
Commit-Here <burhanqibal86@gmail.com>
2021-11-20 18:51:36 +0530

    Added Program to display string using pointers.

--- RightTriangleStar.c
+++ RightTriangleStar.c
@@ -13,7 +13,7 @@ int main()
     int index_ROWS, index_COLS;
     for (index_ROWS = 1; index_ROWS <= 6; index_ROWS++)
     {
-        for (index_COLS = 1; index_COLS <= index_ROWS;index_COLS++)
+        for (index_COLS = 1; index_COLS <= index_ROWS; index_COLS++)
         {
             printf("* ");
         } // for
---
+++ StringWithPointers.c
@@ -0,0 +1,24 @@
+// To display characters of a string using pointers.
+#include<stdio.h>
+
+void printString(char pointerOfC[], int len)
+{
+    
+    puts(pointerOfC);
+} // printString
+
+int main()
+{
+    char c[80];
+    int i = 0, count = 1;
+    printf("Enter the string:\n\t");
+    
+    // can also use gets(c) rather than this loop.
+    while ((c[i] = getchar()) != '\n')
+    {
+        count++;
+        i++;
+    } // while
+
+    printString(c, count);
+} // main
